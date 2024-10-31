9a5f212c502f1bae1078790bee6b1771898ae2ee
Cameron Dahl <camnooten@gmail.com>
2024-10-30 22:28:05 -0500

    Run prettier and delete a couple things

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,14 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     fontFamily: {
-      "display": "Karla",
-      "body": "Inconsolata"
+      display: "Karla",
+      body: "Inconsolata",
     },
     extend: {
       colors: {
-        primary: "#92C7CF"
+        primary: "#92C7CF",
       },
     },
   },
   plugins: [],
-}
-
+};
